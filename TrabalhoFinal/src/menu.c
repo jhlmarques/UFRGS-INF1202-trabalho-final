aac0f778fbbd68a09053a00e5feb27dea67379b6
Matheus Almeida <71467895+matheusalsi@users.noreply.github.com>
2021-04-18 15:25:56 -0300

    menu_versao_funcional

--- TrabalhoFinal/src/menu.c
+++ TrabalhoFinal/src/menu.c
@@ -1,52 +1,37 @@
 #include "menu.h"
 #include "raylib.h"
-
-
-void wait(){
-    int key;
-    do{
-        key = GetKeyPressed();
-    }while(key != 48);
-}
+#include <stdio.h>
 
 int menu(){
-        int op, close = 1;
+        int state;
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+            DrawText("Pressiona o numero:\n 1- NEW GAME\n 2- LOAD GAME\n 3- CREDITS\n 4- EXIT\n", 190, 200, 20, LIGHTGRAY); //Função de Escrita da raylib
+        EndDrawing();
 
-            BeginDrawing();
-            ClearBackground(RAYWHITE);
-                DrawText("Pressiona o numero:\n 1- NEW GAME\n 2- LOAD GAME\n 3- CREDITS\n 4- EXIT\n", 190, 200, 20, LIGHTGRAY);
-            EndDrawing();
+         if(IsKeyPressed(KEY_ONE)){
+            state = 1;
+         }
+        if(IsKeyPressed(KEY_TWO)){
+            state = 2;
+        }
+        if(IsKeyPressed(KEY_THREE)){
+            state = 3;
+        }
+        if(IsKeyPressed(KEY_FOUR)){
+            state = 4;
+        }
+        return state;
+}
 
-            op = GetKeyPressed();
-            switch (op){
-            case 49:
-                BeginDrawing();
-                ClearBackground(RAYWHITE);
-                    DrawText("NEW GAME", 190, 200, 20, RED);
-                    DrawText("Pressione o numero 0 para retornar ao menu principal\n", 190, 400, 20, LIGHTGRAY);
-                EndDrawing();
-                wait();
-                break;
-            case 50:
-                BeginDrawing();
-                ClearBackground(RAYWHITE);
-                    DrawText("LISTA SAVES", 190, 200, 20, RED);
-                    DrawText("Pressione o numero 0 para retornar ao menu principal\n", 190, 400, 20, LIGHTGRAY);
-                EndDrawing();
-                wait();
-                break;
-            case 51:
-                BeginDrawing();
-                ClearBackground(RAYWHITE);
-                    DrawText("CREDITOS\n Jose Henrique Lima Marques\n Matheus Almeida Silva", 190, 200, 20, BLUE);
-                    DrawText("Pressione o numero 0 para retornar ao menu principal\n", 190, 400, 20, LIGHTGRAY);
-                EndDrawing();
-                wait();
-                break;
-            case 52:
-                close = 0;
-                break;
-
-            }
-        return close;
+int show_credits() {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+        DrawText("CREDITOS\n Jose Henrique Lima Marques\n Matheus Almeida Silva", 190, 200, 20, BLUE);
+        DrawText("Pressione o numero ESPACO para retornar ao menu principal\n", 190, 400, 20, LIGHTGRAY);
+    EndDrawing();
+    if(IsKeyPressed(KEY_SPACE))
+        return 1;
+    else
+        return 0;
 }
--- TrabalhoFinal/src/menu.h
+++ TrabalhoFinal/src/menu.h
@@ -2,5 +2,6 @@
 #define MENU_H_INCLUDED
 
     int menu();
+    int show_credits();
 
 #endif // MENU_H_INCLUDED
