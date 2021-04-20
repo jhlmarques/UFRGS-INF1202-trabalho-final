5feb0fcbaed765c03667b4fc8a87bffe200fb787
Matheus Almeida <71467895+matheusalsi@users.noreply.github.com>
2021-04-20 05:35:24 -0300

    alterations_v2
    Alterações no point e protótipo do movimento

--- TrabalhoFinal/src/point.h
+++ TrabalhoFinal/src/point.h
@@ -2,13 +2,6 @@
 #define _point_h_
 #include "raylib.h"
 
-/*
-typedef struct point{
-    unsigned int x;
-    unsigned int y;
-}point, *pPoint;
-*/
-
 void point_move_dir(Vector2 P, int dir);
 
 #endif
